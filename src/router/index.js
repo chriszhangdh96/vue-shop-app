5af0290d0c74601abd685b6fb83845048d399ec7
hmy <qq15137087201@gmail.com>
2020-01-14 09:58:04 +0800

    33

--- src/router/index.js
+++ src/router/index.js
@@ -42,10 +42,7 @@ const routes = [
       {
         path: '/index',
         name:'index',
-<<<<<<< HEAD
         name:'home',
-=======
->>>>>>> b22aef2d701333f59b9ef6d15a63b7a152f58ee8
         component: Index
       },
       {
