c2916bfdff98eb97fd7cde3771ab6023d9a0ca4b
wangshaolong <wangshaolong03@163.com>
2020-01-17 08:48:05 +0800

    wsl地址优化

--- src/views/Address_list.vue
+++ src/views/Address_list.vue
@@ -1,14 +1,22 @@
 <template>
   <div>
     <van-nav-bar title="地址列表" left-text="返回" left-arrow @click-left="onClickLeft" />
-
-    <van-address-list
+ 
+  <van-address-list
       v-model="chosenAddressId"
       :list="list"
       default-tag-text="默认"
       @add="onAdd"
       @edit="onEdit"
     />
+  <!-- <van-pull-refresh v-model="isLoading" @refresh="onRefresh">  
+</van-pull-refresh> -->
+
+     <!-- <van-pagination 
+  v-model="currentPage" 
+  :total-items="24" 
+  :items-per-page="5"
+/> -->
   </div>
 </template>
 <script>
@@ -22,7 +30,9 @@ export default {
         address:'',
         count:0,
       chosenAddressId: "1",
-      list: []
+      list: [],
+      isLoading: false,
+      // urrentPage: 1
     };
   },
 
@@ -40,12 +50,48 @@ export default {
         this.$router.push({
             name:'addressEdit',
             query:{
-               id:item.id
+               _id:item._id
             }
         })
-    }
+    },
+    //  onRefresh() {
+    //   setTimeout(() => {
+    //     this.$toast('刷新成功');
+    //     this.isLoading = false;
+    //    axios.defaults.headers.common["authorization"] =
+    //   "Bearer " + localStorage.getItem("token");
+    // axios
+    //   .get("http://api.cat-shop.penkuoer.com/api/v1/addresses", {
+    //     params: {
+    //       per: "14",
+    //       page: "1"
+    //     }
+    //   })
+    //   .then(res => {
+    //     //   console.group('获取的地址数据')
+    //     //    console.log(res.data.addresses);
+    //     //  console.groupEnd()
+    //     res.data.addresses.forEach(item => {
+    //       axios
+    //         .get(
+    //           "http://api.cat-shop.penkuoer.com/api/v1/addresses/" + item._id,)
+    //         .then(res => {
+    //           console.log(res.data);
+    //             this.list.push({
+    //                 id:this.count+=1,
+    //                 name:res.data.receiver,
+    //                 tel:res.data.mobile,
+    //                 address:res.data.address,
+    //                 _id:res.data._id
+    //             })
+    //         });
+    //     });
+    //   });
+    //   }, 1000);
+    // }
+  
   },
-  mounted() {
+  beforeMount() {
     axios.defaults.headers.common["authorization"] =
       "Bearer " + localStorage.getItem("token");
     axios
@@ -56,11 +102,10 @@ export default {
         }
       })
       .then(res => {
-          console.group('获取的地址数据')
-           console.log(res.data.addresses);
-         console.groupEnd()
+        //   console.group('获取的地址数据')
+        //    console.log(res.data.addresses);
+        //  console.groupEnd()
         res.data.addresses.forEach(item => {
-          
           axios
             .get(
               "http://api.cat-shop.penkuoer.com/api/v1/addresses/" + item._id,)
@@ -70,7 +115,8 @@ export default {
                     id:this.count+=1,
                     name:res.data.receiver,
                     tel:res.data.mobile,
-                    address:res.data.address
+                    address:res.data.address,
+                    _id:res.data._id
                 })
             });
         });
